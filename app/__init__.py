5f3ff662271c2fc887b961b63c2fb6f830697491
Joan_E <joanevans18@gmail.com>
2020-07-13 08:37:01 +0300

    initialized modules, app and app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 def create_app(config_name):
     # initialize app
     app = Flask(__name__, instance_relative_config=True)
 
     # app configurations
     app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+    db.init_app(app)
 
     # register blueprint
     from .main import main as main_bp
