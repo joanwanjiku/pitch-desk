1aa71c1509a7b944a0f97677b7330414b6608742
Joan_E <joanevans18@gmail.com>
2020-07-13 14:38:44 +0300

    Added a user model and its properties

--- app/models.py
+++ app/models.py
@@ -1,5 +1,41 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot access this property')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash,password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update_user(self):
+        db.session.add(self)
+        db.session.commit()
 
 class Category(db.Model):
     __tablename__ = 'category'
@@ -20,7 +56,7 @@ class Pitch(db.Model):
     content = db.Column(db.String(255))
     category_id = db.Column(db.Integer,db.ForeignKey("category.id"))
     posted = db.Column(db.DateTime,default=datetime.utcnow)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def save_pitch(self):
         db.session.add(self)
