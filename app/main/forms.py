f8dce295b3c78261268e6bb73f28d699d8ceab31
Joan_E <joanevans18@gmail.com>
2020-07-13 23:04:41 +0300

    Added a comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,5 +7,9 @@ class PitchForm(FlaskForm):
     title = StringField('Title of your pitch', validators=[Required()])
     content = TextAreaField('Content', validators=[Required()])
     # date = DateTimeField('Date')
-    category = SelectField('Select a category', choices=[('25', 'Job Pitches'), ('26', 'Movie Pitches'), ('27', 'Product Pitches')])
+    category = SelectField('Select a category', choices=[('31', 'Job Pitches'), ('27', 'Movie Pitches'), ('26', 'Product Pitches'), ('32', 'Motivation Pitches')])
     submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Comment')
