07755bc4e4e8f8c6b97d2715da5c7a667522c47b
Joan_E <joanevans18@gmail.com>
2020-07-13 14:51:58 +0300

    configured flask-uploads module

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+
 def create_app(config_name):
     # initialize app
     app = Flask(__name__, instance_relative_config=True)
@@ -13,9 +21,15 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # register blueprint
     from .main import main as main_bp
     app.register_blueprint(main_bp)
+    from .auth import auth as auth_bp
+    app.register_blueprint(auth_bp)
+
+    # configure uploads
+    configure_uploads(app, photos)
 
     return app
