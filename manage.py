b5c3a45f2707e2d8b7f008b2df072ea6c424a7e9
Joan_E <joanevans18@gmail.com>
2020-07-13 07:53:36 +0300

    Added test command

--- manage.py
+++ manage.py
@@ -1,9 +1,24 @@
-from app import create_app
+from app import create_app, db
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import Pitch, Category
 
-app = create_app('development')
+app = create_app('test')
 manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db',MigrateCommand)
+
 manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app= app, db=db, Pitch=Pitch, Category=Category)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
