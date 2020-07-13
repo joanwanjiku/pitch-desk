8bf2d277e73b5645946cf2f32ff664c86f512968
Joan_E <joanevans18@gmail.com>
2020-07-13 15:37:09 +0300

    configured flask-email

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos', IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     # initialize app
@@ -22,6 +24,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     # register blueprint
     from .main import main as main_bp
