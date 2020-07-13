e133c7808bb7dbe131be5ca603a9f770624f58ee
Joan_E <joanevans18@gmail.com>
2020-07-13 07:53:58 +0300

    Config file

--- config.py
+++ config.py
@@ -1,21 +1,25 @@
 import os
 
 class Config:
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfg(Config):
     """
     configurations for prod environment inherits from Config
     """
     pass
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/pitches_test'
 
 class DevConfig(Config):
     """
     configurations for dev environment inherits from Config
     """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:123456@localhost/pitches'
     DEBUG = True
 
 config_options = {
     'development':DevConfig,
-    'production':ProdConfg
+    'production':ProdConfg,
+    'test': TestConfig
 }
